472c84c67ec06622c1f648021f850915a35446ac
ldvv <1210754789@qq.com>
2019-11-20 10:29:06 +0800

    接口

---
+++ src/main/java/com/hi/weiliao/WeiliaoApplication.java
@@ -0,0 +1,12 @@
+package com.hi.weiliao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiliaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeiliaoApplication.class, args);
+	}
+}
