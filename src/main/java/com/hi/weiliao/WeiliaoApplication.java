6648c9937d52b208ea3864e9dd9bd5e14973a126
ldvv <1210754789@qq.com>
2019-11-20 16:32:08 +0800

    修改工程目录

--- src/main/java/com/hi/weiliao/WeiliaoApplication.java
+++ src/main/java/com/hi/weiliao/WeiliaoApplication.java
@@ -2,11 +2,20 @@ package com.hi.weiliao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WeiliaoApplication {
+public class WeiliaoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeiliaoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(WeiliaoApplication.class);
+
 	}
 }
